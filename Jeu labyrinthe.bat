da20a87c4a70afe9b928d85f1de6524235f827b8
Monayy <40573272+Monayy@users.noreply.github.com>
2018-09-03 18:24:43 +0200

    Add files via upload

---
+++ Jeu labyrinthe.bat
@@ -0,0 +1,526 @@
+@echo off
+Title /!\ Attention un virus vient d'etre detecte/!\
+color 1f
+echo                              __xxxxxxxxxxxxxxxx___.
+echo                        _gxXXXXXXXXXXXXXXXXXXXXXXXX!x_
+echo                ,gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx_
+echo              ,gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!_
+echo            _!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!.
+echo          gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXs
+echo        ,!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!.
+echo       g!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo      iXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo     ,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+echo     !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+echo   ,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+echo   !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXi
+echo  dXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo  !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo   XXXXXXXXXXXXXXXXXXXf~~~VXXXXXXXXXXXXXXXXXXXXXXXXXXvvvvvvvvXXXXXXXXXXXXXX!
+echo   !XXXXXXXXXXXXXXXf`       'XXXXXXXXXXXXXXXXXXXXXf`          '~XXXXXXXXXXP
+echo    vXXXXXXXXXXXX!            !XXXXXXXXXXXXXXXXXX!              !XXXXXXXXX
+echo     XXXXXXXXXXv`              'VXXXXXXXXXXXXXXX                !XXXXXXXX!
+echo     !XXXXXXXXX.                 YXXXXXXXXXXXXX!                XXXXXXXXX
+echo      XXXXXXXXX!                 ,XXXXXXXXXXXXXX                VXXXXXXX!
+echo      'XXXXXXXX!                ,!XXXX ~~XXXXXXX               iXXXXXX~
+echo       'XXXXXXXX               ,XXXXXX   XXXXXXXX!             xXXXXXX!
+echo        !XXXXXXX!xxxxxxs______xXXXXXXX   'YXXXXXX!          ,xXXXXXXXX
+echo         YXXXXXXXXXXXXXXXXXXXXXXXXXXX`    VXXXXXXX!s. __gxx!XXXXXXXXXP
+echo          XXXXXXXXXXXXXXXXXXXXXXXXXX!      'XXXXXXXXXXXXXXXXXXXXXXXXX!
+echo          XXXXXXXXXXXXXXXXXXXXXXXXXP        'YXXXXXXXXXXXXXXXXXXXXXXX!
+echo          XXXXXXXXXXXXXXXXXXXXXXXX!     i    !XXXXXXXXXXXXXXXXXXXXXXXX
+echo          XXXXXXXXXXXXXXXXXXXXXXXX!     XX   !XXXXXXXXXXXXXXXXXXXXXXXX
+echo          XXXXXXXXXXXXXXXXXXXXXXXXx_   iXX_,_dXXXXXXXXXXXXXXXXXXXXXXXX
+echo          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXP
+echo          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo           ~vXvvvvXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+echo                    'VXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXvvvvvv~
+echo                      'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX~
+echo                  _    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXv`
+echo                 -XX!  !XXXXXXX~XXXXXXXXXXXXXXXXXXXXXX~   Xxi
+echo                  YXX  '~ XXXXX XXXXXXXXXXXXXXXXXXXX`     iXX`
+echo                  !XX!    !XXX` XXXXXXXXXXXXXXXXXXXX      !XX
+echo                  !XXX    '~Vf  YXXXXXXXXXXXXXP YXXX     !XXX
+echo                  !XXX  ,_      !XXP YXXXfXXXX!  XXX     XXXV
+echo                  !XXX !XX           'XXP 'YXX!       ,.!XXX!
+echo                  !XXXi!XP  XX.                  ,_  !XXXXXX!
+echo                  iXXXx X!  XX! !Xx.  ,.     xs.,XXi !XXXXXXf
+echo                   XXXXXXXXXXXXXXXXX! _!XXx  dXXXXXXX.iXXXXXX
+echo                   VXXXXXXXXXXXXXXXXXXXXXXXxxXXXXXXXXXXXXXXX!
+echo                   YXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXV
+echo                   'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+echo                    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+echo                       VXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+echo                         VXXXXXXXXXXXXXXXXXXXXXXXXXXXXv`
+echo                          ~vXXXXXXXXXXXXXXXXXXXXXXXf`
+echo                              ~vXXXXXXXXXXXXXXXXv~
+echo                                 '~VvXXXXXXXV~~
+echo                                       ~~        
+ping localhost -n 7 > nul
+cls
+color 1f
+pause
+cls
+echo /!\Attention un virus de categorie 8/10 a ete detecter sur votre ordinateur/!\
+ping localhost -n 4 > nul
+cls
+echo Si vous ne le suprimer pas maintenant, votre ordinateur sera en danger !!
+ping localhost -n 4 > nul
+cls
+echo Voulez-vous le suprimer ?
+
+
+set input=
+set /p input=oui/non ?
+
+if %input%==oui goto oui
+if %input%==non goto non
+
+:oui
+echo Vous avez choisi de ne pas le suprimer, effacement des fichier !
+ping localhost -n 7 > nul
+cls
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+cls
+echo Certains de vos fichiers viennent d'etre suprimer !
+ping localhost -n 3 > nul
+pause
+exit
+
+:non
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+dir /s
+cls
+echo Le virus a ete efface ...
+echo Nous vous recommandons un scan de votre ordinateur a l'aide de votre antivirus.
+ping localhost -n 3 > nul
+pause
+exit
+
+
+
+
+
+
+
