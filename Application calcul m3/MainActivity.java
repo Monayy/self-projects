58021474104eaafda8c2330209880f5bfad89e62
Monayy <40573272+Monayy@users.noreply.github.com>
2018-09-09 14:46:21 +0200

    Add files via upload

--- Application calcul m3/MainActivity.java
+++ Application calcul m3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.calculm3.monayy.metrescubes;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
@@ -37,15 +38,20 @@ public class MainActivity extends AppCompatActivity {
                 double n2 = Double.parseDouble(number2.getText().toString());
                 double n3 = Double.parseDouble(number3.getText().toString());
 
-                double res = (n1 * n2 * n3);
+                double res = (Math.floor (n1 * n2 * n3 * 100))/100;
 
                 result.setText(String.valueOf(res));
-            }
+                }
             else {
                     Toast.makeText(view.getContext(),"Please enter the numbers",Toast.LENGTH_SHORT).show();
                 }
 
 
-        };
-    });
-}}
+            };
+        });
+    }
+
+    public void page1 (View view) {
+        startActivity(new Intent(this, page_2.class));
+    }
+}
---
+++ Application calcul m3/page_2.java
@@ -0,0 +1,13 @@
+package com.calculm3.monayy.metrescubes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class page_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_2);
+    }
+}
